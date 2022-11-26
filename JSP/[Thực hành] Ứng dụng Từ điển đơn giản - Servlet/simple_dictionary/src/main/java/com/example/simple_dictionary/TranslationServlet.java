cd90d93017d46d33aa2c9b6be16325cfa172118f
ntm2022 <nguyenminhk2000@gmail.com>
2022-11-26 22:23:24 +0700

    create simple dictionary

---
+++ JSP/[Thực hành] Ứng dụng Từ điển đơn giản - Servlet/simple_dictionary/src/main/java/com/example/simple_dictionary/TranslationServlet.java
@@ -0,0 +1,46 @@
+package com.example.simple_dictionary;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "TranslationServlet", value = "/translate")
+public class TranslationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chào");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("book", "Quyển vở");
+        dictionary.put("computer", "Máy tính");
+
+        //fix lỗi UNICODE
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        String search = request.getParameter("txtSearch");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        String result = dictionary.get(search);
+        if(result != null){
+            writer.println("<h1>Word: " + search + "</h1>");
+            writer.println("<h1>Result: " + result + "</h1>");
+        } else {
+            writer.println("Not found");
+        }
+
+        writer.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
